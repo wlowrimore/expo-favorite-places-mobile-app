42da868027382d9ed3cd4d6b845bfaf613c0927b
William Lowrimore <wlowrimore@gmail.com>
2022-12-29 13:26:33 -0600

    adds a styled title label and title input field

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,11 +1,52 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
 
 function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>THE PLACE FORM</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+
+  label: {
+    fontSize: 18,
+    letterSpacing: 1,
+    marginBotton: 4,
+    color: Colors.lightbg,
+  },
+
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    color: Colors.gray700,
+    borderRadius: 5,
+    backgroundColor: Colors.lightbg,
+  },
+});
--- constants/colors.js
+++ constants/colors.js
@@ -1,11 +1,12 @@
 export const Colors = {
   primary50: "#cfeffd",
   primary100: "#a0defb",
-  primary200: "#5296AD",
+  primary200: "#75B2C6",
   primary400: "#44bdf5",
   primary500: "#78797B",
   primary700: "#0570c9",
   primary800: "#003b88",
   accent500: "#e6b30b",
   gray700: "#242629",
+  lightbg: "#DBDEDF",
 };
