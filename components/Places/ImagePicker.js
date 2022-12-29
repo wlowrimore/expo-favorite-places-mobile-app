0d73626e32dfcd0652f0dd93a273502e7fb72170
William Lowrimore <wlowrimore@gmail.com>
2022-12-29 16:18:29 -0600

    adds custom button for taking Photo

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,11 +1,17 @@
-import { View, Button, Alert } from "react-native";
+import { useState } from "react";
+import { Text, View, Alert, Image, StyleSheet } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from "expo-image-picker";
 
+import OutlinedButton from "../UI/OutlinedButton";
+import { Colors } from "../../constants/colors";
+
 function ImagePicker() {
+  const [pickedImage, setPickedImageu] = useState();
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -39,15 +45,41 @@ function ImagePicker() {
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+
+    setPickedImageu(image.assets[0].uri);
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
-      <Button title="Take Photo" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Photo
+      </OutlinedButton>
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 5,
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
